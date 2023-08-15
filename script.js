ceb31e7b41cdd973d0798f766bab6b7998f76288
Turki <117861968+JUST1TURKI@users.noreply.github.com>
2023-08-15 14:01:45 +0300

    script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ ScrollReveal().reveal('.home-content h1', {origin: 'left'});
 ScrollReveal().reveal('.about-content', {origin: 'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Software Engineering student', 'Data Engineering', 'Data Analyst', 'Data Analytic'],
+    strings: ['Software Engineering student', 'Data Engineering', 'Data Analyst'],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 500,
